bad76aeb8b8d323e5135190f2a277f990c335d3e
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2022-01-03 14:55:16 +0100

    Starting integer version
    Now that our C float version gives identical results to tensorflow's,
    we bootstrap the integer part, through generating additional information
    in the parameter file: per tensor fixed point multiplicands, right shifts,
    and zero points.

--- dump-parameters.py
+++ dump-parameters.py
@@ -23,7 +23,26 @@ else:
     print(f'Usage {sys.argv[0]} (float|int8_t)')
     sys.exit(1)
 
+# Find a fixed point multiplication 
+# Monkey-type translation from tensorflow gemmlowp pipeline 
+def mult_to_fix_shift(multiplicand):
+    assert multiplicand > 0.0, "Multiplicand must be positive"
+    assert multiplicand < 1.0, "Multiplicand must be strictly less than 1"
 
+    rs = 0
+    while multiplicand < 0.5:
+        multiplicand *= 2.0
+        rs += 1
+
+    m = int(round(multiplicand * 2 ** 31))
+
+    if m == 2 ** 31:
+        return m / 2, rs - 1
+    else:
+        return m, rs
+
+    return m, rs
+'''
 # récupération du data-set: i == image, l == label (catégorie)
 (_, _), (test_images, test_labels) = keras.datasets.mnist.load_data()
 
@@ -31,6 +50,7 @@ else:
 test_images = np.pad(array = test_images, pad_width = 2, mode = 'constant', constant_values = 0)
 test_images = np.expand_dims(test_images, axis=-1)
 test_images = test_images[2:test_images.shape[0] - 2]
+'''
 
 model_filename = f'lenet5_models/model_{rep}.tflite' 
 interpreter = tf.lite.Interpreter(model_path = model_filename)
@@ -61,8 +81,25 @@ if True:
             else :
                 continue
 
+            # Biases do not need adjustments, this is done in the preceeding conv2d
+            if rep == 'int8_t' and layer_name[2] != 'BiasAdd':
+                t = "/* Quantization:\n  " +  str(layer['quantization_parameters']) + "\n*/\n"
+                t += rep + ' ' + layer_name[1]
+                t += f"_scales[{len(layer['quantization_parameters']['scales'])}][2]"
+                t += " = {\n"
+                for scale in layer['quantization_parameters']['scales']:
+                    t += '  {' + str(mult_to_fix_shift(scale)).strip('()') + '},'
+                t += '\n};\n'
+                t += rep + ' ' + layer_name[1]
+                t += f"_zero_points[{len(layer['quantization_parameters']['zero_points'])}]"
+                t += " = {\n  "
+                for zp in layer['quantization_parameters']['zero_points']:
+                    t += str(zp) + ','
+                t += '\n};\n'
+            else:
+                t = ""
             layer_shape = re.sub(' +', '][', re.sub('\[ +', '[', f'{layer["shape"]}'))
-            t = f'{rep} {layer_name [1]}_{wtype}' + f'{layer_shape} =\n'
+            t += f'{rep} {layer_name[1]}_{wtype}' + f'{layer_shape} =\n'
             s = np.array2string(interpreter.get_tensor(layer_idx), 80, separator=',')
             s = s.replace('[', '{')
             s = s.replace(']', '}')
